b738ec83762367ac50460d74f54c0c256084a8da
xHernanL <nano_manya07@hotmail.com>
2020-09-23 08:42:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appdata/DataActivity.java
@@ -0,0 +1,15 @@
+package com.example.appdata;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_up);
+    }
+}
