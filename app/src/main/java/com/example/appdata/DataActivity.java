41032f614729c748c6ac6a9d73b0094a182ef22e
xHernanL <nano_manya07@hotmail.com>
2020-09-24 11:59:29 -0300

    Upload 24/09

--- app/src/main/java/com/example/appdata/DataActivity.java
+++ app/src/main/java/com/example/appdata/DataActivity.java
@@ -2,15 +2,35 @@ package com.example.appdata;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DataActivity extends AppCompatActivity {
+
+    TextView tvInfoEnviada;
+    TextView tvPhone;
+    TextView tvDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_up);
 
+        tvInfoEnviada = (TextView)findViewById(R.id.tv1);
+        tvPhone = (TextView)findViewById(R.id.tv2);
+        tvDate = (TextView)findViewById(R.id.tvF);
+
+        Bundle bundle = getIntent().getExtras();
+        Bundle bundle1 = getIntent().getExtras();
+        Bundle bundleF = getIntent().getExtras();
+
+        String dato = bundle.getString("Info").toString();
+        String datoF = bundleF.getString("datef".toString());
+        String dato1 = bundle1.getString("phone".toString());
+
+        tvInfoEnviada.setText("Nombre completo: " + dato);
+        tvDate.setText("Fecha de nacimiento: "+datoF);
+        tvPhone.setText("Telefono: " + dato1);
     }
 }
--- app/src/main/java/com/example/appdata/MainActivity.java
+++ app/src/main/java/com/example/appdata/MainActivity.java
@@ -9,63 +9,86 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
-
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-        private static final String TAG = "MainActivity";
-        private TextView mDisplayDate;
-        private DatePickerDialog.OnDateSetListener mDateSetListener;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    Button btnSig;
+    EditText etName;
+    EditText etPhone;
+    TextView tvDate;
 
 
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            mDisplayDate = (TextView) findViewById(R.id.tvDate);
+    private static final String TAG = "MainActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
-            mDisplayDate.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Calendar cal = Calendar.getInstance();
-                    int year = cal.get(Calendar.YEAR);
-                    int month = cal.get(Calendar.MONTH);
-                    int day = cal.get(Calendar.DAY_OF_MONTH);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-                    DatePickerDialog dialog = new DatePickerDialog(
-                            MainActivity.this,
-                            android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                            mDateSetListener,
-                            year,month,day);
-                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    dialog.show();
-                }
-            });
-
-            mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                    month = month + 1;
-                    Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
-
-                    String date = month + "/" + day + "/" + year;
-                    mDisplayDate.setText(date);
-                }
-            };
-
-        }
-
-        public void onClick(View view){
-            Intent myIntent=new Intent(MainActivity.this,DataActivity.class);
-            startActivity(myIntent);
-        }
+        btnSig = (Button)findViewById(R.id.btn_next);
+        etName = (EditText)findViewById(R.id.etName);
+        etPhone = (EditText)findViewById(R.id.etPhone);
+        tvDate = (TextView)findViewById(R.id.tvDate);
 
 
+        btnSig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DataActivity.class);
+                intent.putExtra("Info", etName.getText().toString());
+                intent.putExtra("phone",etPhone.getText().toString());
+                intent.putExtra("datef",tvDate.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        mDisplayDate = (TextView) findViewById(R.id.tvDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
+
+    public void onClick(View View){
+        Intent myIntent=new Intent(MainActivity.this,DataActivity.class);
+
+        startActivity(myIntent);
+    }
+
+}
+
 
 
